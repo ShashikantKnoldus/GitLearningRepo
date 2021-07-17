5314c7d46e7f4ccbfa92030041e2515e3f628162
ShashikantKnoldus <shashikant.tyagi@knoldus.com>
2021-07-17 13:37:29 +0530

    for loop added

---
+++ forLoops.java
@@ -0,0 +1,9 @@
+class forLoops{
+    public static void main(string args[]){
+        int a=1;
+        for(int i=1;i<=100;i++){
+            a=a+i;
+        }
+        System.out.println(a);
+    }
+}
